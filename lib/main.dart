d4fdd7e71d0f804c7ea730880152c3ff458c16fd
zolbayar8027 <zolbayardoll1234@gmail.com>
2021-03-30 19:23:12 +0800

    state.......

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import 'some_ineedtodoit/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,42 +23,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BodyWidget extends StatelessWidget {
-
-  //String fruit = 'Apples';
-  //final fruitNotfier = ValueNotifier<String>('apples');
-  final colorNotfier = ValueNotifier<Color>(Colors.tealAccent.shade700);
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: colorNotfier,
-      builder: (context, colot, child) {
-        return Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.red,
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: 100),
-                Text('hello'),
-                SizedBox(height: 100),
-                ElevatedButton(
-                  onPressed: () {
-                    print('I was clicked.');
-
-                    colorNotfier.value = Colors.indigo.shade700;
-                  },
-                  child: Text('Click me'),
-                ),
-              ],
-            ),
-          ),
-
-        );
-      },
-
-    );
-  }
-}
+// TODO: When you press the button, change the background color of the app
