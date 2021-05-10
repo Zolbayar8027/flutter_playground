61a0fabd02ca21164363efdccdaa13c66ffafec7
zolbayar8027 <zolbayardoll1234@gmail.com>
2021-05-10 14:58:44 +0800

    5/10 homework

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,44 @@
-
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mongol/mongol.dart';
 
-import 'authentication_service.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: AuthenticationWrapper());
-  }
-}
-
-class AuthenticationWrapper extends StatelessWidget {
-  final _authService = AuthenticationService(FirebaseAuth.instance);
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User>(
-      stream: _authService.authStateChanges,
-      builder: (context, snapshot) {
-        final user = snapshot.data;
-        if (user == null) {
-          return SignInPage(authService: _authService);
-        } else {
-          return HomePage(
-            authService: _authService,
-          );
-        }
-      },
-    );
-  }
-}
-
-class SignInPage extends StatelessWidget {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final AuthenticationService authService;
-
-  SignInPage({Key key, this.authService});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            controller: emailController,
-            decoration: InputDecoration(
-              labelText: "Email",
-            ),
-          ),
-          TextField(
-            controller: passwordController,
-            decoration: InputDecoration(
-              labelText: "Password",
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              authService.signIn(
-                email: emailController.text,
-                password: passwordController.text,
-              );
-            },
-            child: Text('Sign in'),
-          ),
-        ],
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
 
 class HomePage extends StatelessWidget {
-  final AuthenticationService authService;
-
-  HomePage({Key key, this.authService});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
+
       body: Center(
-        child: Column(
-          children: [
-            Text('HOME'),
-            ElevatedButton(
-              onPressed: () {
-                authService.signOut();
-              },
-              child: Text('Log out'),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fitHeight,
+              image: AssetImage('images/profile.jpg',),
+
+            )
+          ),
+          height: 500,
+          width: 400,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: MongolText(
+              'ᠮᠢᠨᠦ ᠨᠡᠷ᠎ᠡ ᠶᠢ ᠵᠣᠯᠪᠠᠶᠠᠷ ᠭᠡᠳᠡᠭ᠃ 21 ᠨᠠᠰᠤᠲᠠᠢ ᠪᠥᠭᠡᠳ ᠡᠮ ᠠᠢ ᠤᠤ ᠳᠡᠭᠡᠳᠦ ᠰᠤᠷᠭᠠᠭᠤᠯᠢ ᠳᠤ 3 ᠳᠦᠭᠡᠷ ᠺᠦᠷᠰ ᠲᠦ ᠰᠤᠷᠠᠳᠠᠭ ᠪᠣᠯᠤᠨ᠎ᠠ᠃ ᠪᠢ ᠴᠢᠯᠦᠭᠡᠲᠦ ᠴᠠᠭᠠᠷᠠᠢ ᠠᠭᠤᠯᠠᠨ ᠳᠤ ᠭᠠᠷᠬᠤ ᠳᠤᠷᠠᠲᠠᠢ ᠪᠠᠰᠠ ᠮᠠᠰᠢᠨ ᠪᠠᠷᠢᠬᠤ᠃',
+              style: TextStyle(fontFamily: 'MongolianScript', fontSize: 15, color: Colors.white),
+
             ),
-          ],
+          ),
         ),
+      ),
       ),
     );
   }
